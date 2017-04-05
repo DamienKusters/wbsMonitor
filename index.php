f9977f50897771198a6d8b162ae4e4600063ca21
Kevinoomen <kevinx2ultimate@hotmail.com>
2017-04-05 13:31:59 +0200

    layout deel1
    table gemaakt met de goede invul velden

--- index.php
+++ index.php
@@ -1,54 +1,84 @@
 <html>
-<head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link href="css/bootstrap.min.css" rel="stylesheet">
-</head>
-<body>
-  <button type="button" class="btn btn-lg btn-default">Default</button>
-        <button type="button" class="btn btn-lg btn-primary">Primary</button>
-        <button type="button" class="btn btn-lg btn-success">Success</button>
-        <button type="button" class="btn btn-lg btn-info">Info</button>
-        <button type="button" class="btn btn-lg btn-warning">Warning</button>
-        <button type="button" class="btn btn-lg btn-danger">Danger</button>
-        <button type="button" class="btn btn-lg btn-link">Link</button>
-        <div class="row">
-        <div class="col-md-6">
-          <table class="table table-striped">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>O</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-              </tr>
-            </tbody>
-          </table>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container-fluid">
+            <div class="row HeaderRow">
+                <div class="col-md-5">
+                    <img class="img-responsive icon" src="img/icon.png"></a>
+                </div>
+                <div class="col-md-7"></div>
+            </div>
         </div>
-      </div>
-
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-</body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <table class="table table-striped">
+                        <thead>
+                            <tr>
+                                <th>nr.</th>
+                                <th>Task</th>
+                                <th>Predecessor</th>
+                                <th>Who?</th>
+                                <th>MoSCoW</th>
+                                <th>Plan (Min.)</th>
+                                <th>Do (Min.)</th>
+                                <th>Check</th>
+                                <th>Act</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><textarea class="form-control"></textarea></td>
+                                <td><textarea class="form-control"></textarea></td>
+                                <td><input class="btn btn-success" type="button" value="Start" /></td>
+                                <td><input class="btn btn-warning" type="button" value="Taak verwijderen"/></td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><textarea class="form-control"></textarea></td>
+                                <td><textarea class="form-control"></textarea></td>
+                                <td><input class="btn btn-success" type="button" value="Start" /></td>
+                                <td><input class="btn btn-warning" type="button" value="Taak verwijderen"/></td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><input class="form-control" type="text" /></td>
+                                <td><textarea class="form-control"></textarea></td>
+                                <td><textarea class="form-control"></textarea></td>
+                                <td><input class="btn btn-success" type="button" value="Start" /></td>
+                                <td><input class="btn btn-warning" type="button" value="Taak verwijderen"/></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+    </body>
 </html>
