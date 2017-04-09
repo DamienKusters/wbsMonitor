2c6a1fcd1347a3bae3be82c54550c97b97b74774
TheNocturnalDeveloper <jakevanhout@outlook.com>
2017-04-09 23:18:09 +0200

    scrollbar glitch table gefixed, begonnen functie genereren table rows

--- js/default.js
+++ js/default.js
@@ -1,3 +1,41 @@
+                            /*<tr>
+                                <td class="VerticalALign Number">1</td>
+                                <td class="VerticalALign Task"><input class="form-control" type="text" /></td>
+                                <td class="VerticalALign Predecessor"><input class="form-control" type="number" /></td>
+                                <td class="VerticalALign Who">
+                                     <select class="fsSelect" data-width="100px">
+                                     </select>
+                                </td>
+                                <td class="VerticalALign MoSCoW">
+                                    <select class="fsSelect" data-width="100px">
+                                        <option value="Must">Must</option>
+                                        <option value="Could">Could</option>
+                                        <option value="Should">Should</option>
+                                        <option value="Won't">Won't</option>
+                                    </select>
+                                </td>
+                                <td class="VerticalALign Plan"><input class="form-control" type="text" /></td>
+                                <td class="VerticalALign Do"><input class="form-control" type="text" /></td>
+                                <td class="VerticalALign Check"><textarea class="form-control"></textarea></td>
+                                <td class="VerticalALign Act"><textarea class="form-control"></textarea></td>
+                                <td class="VerticalALign Start"><input class="btn btn-success" type="button" value="Start" /></td>
+                                <td class="VerticalALign Remove"><input class="btn btn-warning" type="button" value="Remove"/></td>
+                            </tr>*/
+
+
+const createTaskRow = data => $("<tr></tr>")
+    .append($("<td></td>").attr("class", "verticalAlign Number"))
+    .append($("<td></td>").attr("class", "verticalAlign Task"))
+    .append($("<td></td>").attr("class", "verticalAlign Predecessor"))
+    .append($("<td></td>").attr("class", "verticalAlign "))
+    .append($("<td></td>").attr("class", "verticalAlign Number"))
+    .append($("<td></td>").attr("class", "verticalAlign Number"))
+    .append($("<td></td>").attr("class", "verticalAlign Number"))
+    .append($("<td></td>").attr("class", "verticalAlign Number"))
+    .append($("<td></td>").attr("class", "verticalAlign Number"))
+    .append($("<td></td>").attr("class", "verticalAlign Number"))
+    .append($("<td></td>").attr("class", "verticalAlign Number"));
+
 $(document).ready(() => {
 
 $(".fsSelect").select();
