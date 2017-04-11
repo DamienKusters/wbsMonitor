d80f89e73a0f0483198ecc7a418e0007f277c679
Unknown <damien.kusters@ziggo.nl>
2017-04-11 21:50:49 +0200

    Swapped some code in default.js

--- js/default.js
+++ js/default.js
@@ -1,26 +1,26 @@
-                            /*<tr>
-                                <td class="VerticalALign Number">1</td>
-                                <td class="VerticalALign Task"><input class="form-control" type="text" /></td>
-                                <td class="VerticalALign Predecessor"><input class="form-control" type="number" /></td>
-                                <td class="VerticalALign Who">
-                                     <select class="fsSelect" data-width="100px">
-                                     </select>
-                                </td>
-                                <td class="VerticalALign MoSCoW">
-                                    <select class="fsSelect" data-width="100px">
-                                        <option value="Must">Must</option>
-                                        <option value="Could">Could</option>
-                                        <option value="Should">Should</option>
-                                        <option value="Won't">Won't</option>
-                                    </select>
-                                </td>
-                                <td class="VerticalALign Plan"><input class="form-control" type="text" /></td>
-                                <td class="VerticalALign Do"><input class="form-control" type="text" /></td>
-                                <td class="VerticalALign Check"><textarea class="form-control"></textarea></td>
-                                <td class="VerticalALign Act"><textarea class="form-control"></textarea></td>
-                                <td class="VerticalALign Start"><input class="btn btn-success" type="button" value="Start" /></td>
-                                <td class="VerticalALign Remove"><input class="btn btn-warning" type="button" value="Remove"/></td>
-                            </tr>*/
+/*<tr>
+    <td class="VerticalALign Number">1</td>
+    <td class="VerticalALign Task"><input class="form-control" type="text" /></td>
+    <td class="VerticalALign Predecessor"><input class="form-control" type="number" /></td>
+    <td class="VerticalALign Who">
+         <select class="fsSelect" data-width="100px">
+         </select>
+    </td>
+    <td class="VerticalALign MoSCoW">
+        <select class="fsSelect" data-width="100px">
+            <option value="Must">Must</option>
+            <option value="Could">Could</option>
+            <option value="Should">Should</option>
+            <option value="Won't">Won't</option>
+        </select>
+    </td>
+    <td class="VerticalALign Plan"><input class="form-control" type="text" /></td>
+    <td class="VerticalALign Do"><input class="form-control" type="text" /></td>
+    <td class="VerticalALign Check"><textarea class="form-control"></textarea></td>
+    <td class="VerticalALign Act"><textarea class="form-control"></textarea></td>
+    <td class="VerticalALign Start"><input class="btn btn-success" type="button" value="Start" /></td>
+    <td class="VerticalALign Remove"><input class="btn btn-warning" type="button" value="Remove"/></td>
+</tr>*/
 
 
 const createTaskRow = data => $("<tr></tr>")
@@ -36,21 +36,16 @@ const createTaskRow = data => $("<tr></tr>")
     .append($("<td></td>").attr("class", "verticalAlign Number"))
     .append($("<td></td>").attr("class", "verticalAlign Number"));
 
-$(document).ready(() => {
-
-$(".fsSelect").select();
-
-
+$(document).ready(() =>
+{
+  $(".fsSelect").select();
+  $('#login-trigger').click(function(){
+      $(this).next('#login-content').slideToggle();
+      $(this).toggleClass('active');
+      if ($(this).hasClass('active')){
+          $(this).find('span').html('&#x25B2;');
+      }else{
+          $(this).find('span').html('&#x25BC;');
+      }
 });
-
-$(document).ready(function(){
-    $('#login-trigger').click(function(){
-        $(this).next('#login-content').slideToggle();
-        $(this).toggleClass('active');
-        if ($(this).hasClass('active')){
-            $(this).find('span').html('&#x25B2;');
-        }else{
-            $(this).find('span').html('&#x25BC;');
-        }
-    });
 });
