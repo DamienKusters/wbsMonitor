ebab161e44daa2f029222bdde7df6a4878072d68
Kevinoomen <kevinx2ultimate@hotmail.com>
2017-11-01 09:39:43 +0100

    bootsrap begone!

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="css/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
         <link rel="stylesheet" href="css/jquery-select/css/jquery.select.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
         <link href="css/default.css" rel="stylesheet">
@@ -34,7 +33,7 @@
                         <p class="text">Username</p><input name="username" class="text" type="text" />
                         <p class="text">Password</p><input name="password" class="text" type="password" />
                         <a id="submit" class="waves-effect waves-light btn" style=" float:right; color: black; background-color: white;">Login</a>
-                        <br />
+                        
                     </form>
                 </div>
             </div>
@@ -107,7 +106,6 @@
         </div>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="js/bootstrap.min.js"></script>
         <script src="css/jquery-select/js/jquery.select.js"></script>
         <script src="js/default.js"></script>
         <!-- Timer plugin -->
