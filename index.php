f02ce92f083a93c9d196cda3165ba74bb5cd16a3
Kevinoomen <kevinx2ultimate@hotmail.com>
2017-11-01 10:13:10 +0100

    button fix

--- index.php
+++ index.php
@@ -33,7 +33,6 @@
                         <p class="text">Username</p><input name="username" class="text" type="text" />
                         <p class="text">Password</p><input name="password" class="text" type="password" />
                         <a id="submit" class="waves-effect waves-light btn" style="margin-bottom: 10px; float:right; color: black; background-color: white;">Login</a>
-                        
                     </form>
                 </div>
             </div>
@@ -43,8 +42,8 @@
               <div id="controlPanel">
                 <form>
                     <h2 id="lblProjectTitle">Project name here...</h2>
-                    <input id="btnNewProject" class="btn btn-success" type="button" value="New Project"/>
-                    <input id="btnShowProjects" class="btn btn-success" type="button" value="Show Projects"/>
+                    <input id="btnNewProject" class="btn btn-success" type="button" style="margin-left: 10px;" value="New Project"/>
+                    <input id="btnShowProjects" class="btn btn-success" type="button" style="margin-left: 10px;" value="Show Projects"/>
                 </form>
               </div>
             </div>
